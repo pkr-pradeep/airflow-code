7a79030072ab5279b650def76a55333bd1be6e04
Pradeep <pradeeprout56@gmail.com>
2022-01-27 11:33:40 +0530

    3rd airflow commit

--- dags/user-processing.py
+++ dags/user-processing.py
@@ -1,6 +1,7 @@
 from airflow.models import DAG
 from datetime import datetime
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.http.sensors.http import HttpSensor
 
 
 default_args = {
@@ -15,10 +16,16 @@ with DAG('user_processing', schedule_interval='@daily',
         task_id='creating_table',
         sqlite_conn_id='db_sqlite',
         sql='''
-        CREATE TABLE user_travel_info (
+            CREATE TABLE user_travel_info (
             aadhar_no INT PRIMARY KEY,
             travel_date DATETIME,
             mode TEXT
-        )
-    '''
+            );
+            '''
+    )
+
+    is_api_availble = HttpSensor(
+        task_id = 'is_api_available',
+        http_conn_id = 'user_api',
+        endpoint='traveldata/'
     )
