46c3fca5b1a85e056e917e98519284895f7e307f
pkr-pradeep <pradeeprout56@gmail.com>
2022-02-04 14:39:41 +0530

    4 feb 2022 airflow data 2nd commit

--- dags/covid-file-sensor.py
+++ dags/covid-file-sensor.py
@@ -17,13 +17,15 @@ default_args = {
     'start_date': datetime(2022, 1, 31)
 }
 
+
 def hook_the_db(**context):
-    db_hook =  SqliteHook("");
+    db_hook = SqliteHook("")
+
 
 def hook_the_data(**context):
-    hook = FSHook("conn_filesensor_Odisha");
-    full_path=[os.path.join(hook.get_path(), f'Covid_{state}.txt') 
-                for state in ['Odisha', 'Gujarat', 'UttarPradesh']]
+    hook = FSHook("conn_filesensor_Odisha")
+    full_path = [os.path.join(hook.get_path(), f'Covid_{state}.txt')
+                 for state in ['Odisha', 'Gujarat', 'UttarPradesh']]
     print(full_path)
     for path in full_path:
         if os.path.isfile(path):
@@ -32,26 +34,36 @@ def hook_the_data(**context):
                 d = json.loads(file.read())
                 context['ti'].xcom_push(key=os.path.splitext(base)[0], value=d)
 
+
 def pull_state_data(**context):
     list_state = ['Odisha', 'Gujarat', 'UttarPradesh']
     for state in list_state:
-        state_travel_data = context['ti'].xcom_pull(key=state)
-        if not len(state_travel_data):
-            raise ValueError('value not found for ',state)
-        # Convert string to Python dict
-        travel_list = json.loads(state_travel_data)
-        #for i in len(employee_dict):
-        for x in range(len(travel_list)):
-	        print(travel_list[x]['aadhar_no'])
+     state_travel_data = context['ti'].xcom_pull(key=state)
+     if not len(state_travel_data):
+      raise ValueError('value not found for ', state)
+     # Convert string to Python dict
+     travel_list = json.loads(state_travel_data)
+     for x in range(len(travel_list)):
+      processed_user = json_normalize({
+          'aadhar_no': travel_list[x]['aadhar_no'],
+          'travel_date' : travel_list[x]['travel_date'],
+          'mode' : travel_list[x]['mode'],
+          'state' : state
+      })
+      print(processed_user)
 
 def data_process():
     pass
+
 def db_store():
     pass
+
+
 def _failure_callback(context):
     if isinstance(context['exception'], AirflowSensorTimeout):
         print(context)
     print("Sensor timed out")
+
 
 def m_sent_email_alert(**context):
     print('preparing to send mail')
@@ -67,6 +79,7 @@ def m_sent_email_alert(**context):
         )
         email_op.execute(context)
 
+
 with DAG('covid_2019_travel_data_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
     creating_table = SqliteOperator(
@@ -81,18 +94,18 @@ with DAG('covid_2019_travel_data_dag', schedule_interval='@daily', default_args=
             );
             '''
     )
-    
+
     states = [
         FileSensor(
-        task_id=f'covid_{state}',
-        retries=3,
-        poke_interval=10,
-        timeout=60,
-        mode="reschedule",
-        on_failure_callback=m_sent_email_alert,
-        filepath=f'Covid_{state}.txt',
-        fs_conn_id=f'conn_filesensor_{state}'
-    ) for state in ['Odisha', 'Gujarat', 'UttarPradesh']]
+            task_id=f'covid_{state}',
+            retries=3,
+            poke_interval=10,
+            timeout=60,
+            mode="reschedule",
+            on_failure_callback=m_sent_email_alert,
+            filepath=f'Covid_{state}.txt',
+            fs_conn_id=f'conn_filesensor_{state}'
+        ) for state in ['Odisha', 'Gujarat', 'UttarPradesh']]
 
     process = PythonOperator(
         task_id="process",
